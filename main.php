35c5d15dfe7bbf1daa47967e01de23951f98887c
ehab akl <ehabakl2023@mail.com>
2023-03-25 13:40:41 +0200

    first commit

---
+++ main.php
@@ -0,0 +1,26 @@
+<?php
+    phpinfo();
+    echo "<br>";
+    define('name','Welcome to my Website');
+    echo name;
+    echo "<br>";
+    print_r ($_SERVER)  ;
+    echo "<br>";
+    echo __FILE__ ;
+    echo "<br>";
+    $age = 10 ;
+    switch($age){
+        case $age<5 :
+            echo "Stay home" ;
+            break;
+        case "5" :
+            echo "Go to the  kindergarden";
+            break;
+        default : 
+            if ($age>=6 && $age<=12){
+                $age=$age-4;
+                echo "Go to Grade : $age ";
+            }
+    }
+
+?>
